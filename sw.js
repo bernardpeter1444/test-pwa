2d8f933f555f50a4ccd8ef63eeeeb22fe8512680
bernardpeter1444 <bernard.peter@gmail.com>
2025-09-09 15:10:42 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,22 @@
-const CACHE_NAME = 'test-pwa-cache-v1';
+const CACHE_NAME = 'dienchan-cache-v1';
 const urlsToCache = [
   '/',
-  '/index.html',
+  '/Index.html',
   '/manifest.json',
-  '/style.css',
   '/script.js',
-  '/icon-192.png',
-  '/icon-512.png'
+  '/icon.png',
+  '/icon.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
   self.skipWaiting();
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
